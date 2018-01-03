f537d5d55698f78cbb85dfe7b68ac5191174a636
Patrycja <patrycjalucja@gmail.com>
2018-01-03 14:52:31 +0100

    added correction checking

--- implementation/Parser.py
+++ implementation/Parser.py
@@ -3,7 +3,18 @@ class Parser:
         print("Checking format correction...")
         if not self.checkCorrection(newick):
             print ("Wrong format. Please try again.")
+        else:
+            print("Format ok.")
 
     def checkCorrection(self, newick):
-        print("#TODO")
+        brackets = 0
+        for i in newick:
+            if i == '(':
+                brackets = brackets + 1
+            elif i == ')':
+                if brackets <= 0:
+                    return False
+                brackets = brackets - 1
+        if brackets is not 0 or not newick.strip().endswith(';'):
+            return False
         return True
